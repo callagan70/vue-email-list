b56b4f07608e6d500b2622b315044b78df742f6e
Angelo Melotti <96485963+callagan70@users.noreply.github.com>
2022-04-09 14:45:05 +0100

    start

---
+++ main.js
@@ -0,0 +1,21 @@
+// VueJS
+
+var app = new Vue({
+    el: '#root',
+// SECTION DATA
+    data: {
+     
+    },
+// !SECTION DATA fine
+
+    created:{
+
+
+    },
+
+// SECTION Methods
+    methods: {
+
+    },
+// !SECTION Methods
+  });
